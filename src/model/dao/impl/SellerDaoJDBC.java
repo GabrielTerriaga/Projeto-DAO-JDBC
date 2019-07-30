5ada3c13a58a944fdddb196e303f48e4577502b6
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-07-30 11:53:14 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -14,71 +14,82 @@ import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
-	//Injecao de dependencia
+	// Injecao de dependencia
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}//
-	
+
 	@Override
 	public void insert(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		PreparedStatement st = null; //leva ao banco a consulta sql ou sql
-		ResultSet rs = null; //trazendo os dados do DB
+		PreparedStatement st = null; // leva ao banco a consulta sql ou sql
+		ResultSet rs = null; // trazendo os dados do DB
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+
 			st.setInt(1, id);
-			rs = st.executeQuery(); //resultSet recebe a Query que foi enviado e executado no DB
-			//testar se a consulta trouxe alguma coisa e transformar de table para obj na memoria, para ter referencia
+			rs = st.executeQuery(); // resultSet recebe a Query que foi enviado e executado no DB
+			// testar se a consulta trouxe alguma coisa e transformar de table para obj na
+			// memoria, para ter referencia
 			if (rs.next()) {
-				//Department
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				//Seller
-				Seller sel = new Seller();
-				sel.setId(rs.getInt("Id"));
-				sel.setName(rs.getString("Name"));
-				sel.setEmail(rs.getString("Email"));
-				sel.setBirthDate(rs.getDate("BirthDate"));
-				sel.setBaseSalary(rs.getDouble("BaseSalary"));
-				sel.setDepartment(dep);
+				// Department
+				Department dep = instantiateDepartment(rs);
+				// Seller
+				Seller sel = instantiateSeller(rs, dep);
 				return sel;
 			}
 			System.out.println("ID invalido!");
 			return null;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
+
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller sel = new Seller();
 		
+		sel.setId(rs.getInt("Id"));
+		sel.setName(rs.getString("Name"));
+		sel.setEmail(rs.getString("Email"));
+		sel.setBirthDate(rs.getDate("BirthDate"));
+		sel.setBaseSalary(rs.getDouble("BaseSalary"));
+		sel.setDepartment(dep);
+		
+		return sel;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		
+		return dep;
 	}
 
 	@Override
